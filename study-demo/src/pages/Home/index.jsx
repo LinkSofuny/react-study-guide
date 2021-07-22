308ad3fcd081b17f212fdb6f490d9479d04d5472
Link <cyd551646@sina.com>
2021-07-22 16:47:02 +0800

    路由

--- study-demo/src/pages/Home/index.jsx
+++ study-demo/src/pages/Home/index.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import qs from 'qs'
+
 export default class Home extends Component {
     render() {
-        const { search } = this.props.location
-        const res = qs.parse(search.substring(1))
         return (
             <div>
-                Home: {res.totalData}
+                Home
             </div>
         )
     }
