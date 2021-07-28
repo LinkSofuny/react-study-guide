82c7407a7967475a6781df487926f80fcd9bfa38
Link <cyd551646@sina.com>
2021-07-28 14:04:56 +0800

    完成扩展

--- study-demo/test-Demo/src_hook复习/pages/UseRef/index.jsx
+++ study-demo/test-Demo/src_hook复习/pages/UseRef/index.jsx
@@ -1,44 +1,44 @@
 import React, { Component, useState, useRef } from 'react'
 
 // 函数- 如果以use开头， React 会认为你使用了自定义hook
-// function RefHook() {
+function RefHook() {
     
-//     const [count, setCount] = useState(0)
-//     const countRef = useRef()
-//     const headerRef = useRef()
-//     function showInfo () {
-//         console.log('countRef', countRef)
-//         console.log('headerRef', headerRef)
-//     }
-//     return (
-//         <div>
-//             <h4>Home Component</h4>
-//             <h5 ref={headerRef}>header</h5>
-//             <input type="text" ref={countRef} onChange={showInfo}/>
-//         </div>
-//     )
-// }
+    const [count, setCount] = useState(0)
+    const countRef = useRef()
+    const headerRef = useRef()
+    function showInfo () {
+        console.log('countRef', countRef)
+        console.log('headerRef', headerRef)
+    }
+    return (
+        <div>
+            <h4>Home Component</h4>
+            <h5 ref={headerRef}>header</h5>
+            <input type="text" ref={countRef} onChange={showInfo}/>
+        </div>
+    )
+}
 
 // 对照(类)
-class RefHook extends Component{
+// class RefHook extends Component{
 
-    state = {
-        count: 0
-    }
-    showInfo = () => {
-        console.log('countRef', this.refs.countRef)
-        console.log('headerRef', this.refs.headerRef)
-    }
-    render() {
-        return (
-            <div>
-                <h4>Home Component</h4>
-                <h5  ref='headerRef' >header</h5>
-                <input type="text" ref='countRef' onChange={this.showInfo}/>
-            </div>
-        )
-    }
-}
+//     state = {
+//         count: 0
+//     }
+//     showInfo = () => {
+//         console.log('countRef', this.refs.countRef)
+//         console.log('headerRef', this.refs.headerRef)
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h4>Home Component</h4>
+//                 <h5  ref='headerRef' >header</h5>
+//                 <input type="text" ref='countRef' onChange={this.showInfo}/>
+//             </div>
+//         )
+//     }
+// }
 
 
 export default RefHook
